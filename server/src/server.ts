4cd9b16574e4503fe168779273ba46221394d3b1
guicostaarantes <arantes.g@outlook.com>
2021-01-10 10:27:38 -0300

    ♻️ put db file in git ignored folder

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,9 @@
-import path from "path";
 import App from "@/app";
 import SQLiteDatabaseProvider from "@/providers/database/impl/SQLiteDatabaseProvider";
 import BcryptHashProvider from "@/providers/hash/impl/BcryptHashProvider";
 import RandTokenProvider from "@/providers/token/impl/RandTokenProvider";
 
-const database = new SQLiteDatabaseProvider(
-  path.join(__dirname, "..", "db.sqlite3"),
-);
+const database = new SQLiteDatabaseProvider(process.env.DBPATH);
 const hash = new BcryptHashProvider(8);
 const token = new RandTokenProvider(64);
 
